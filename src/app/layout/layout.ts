ffce3114dca533e26bd9f01d3e8a028c69764b52
Jean-Philippe Maquestiaux <maquejp@gmail.com>
2026-02-13 13:54:38 +0100

    Component: About me Fixes #10

--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -3,11 +3,12 @@ import { Hero } from './hero/hero';
 
 import { personalInfo } from '@data';
 import { PersonalInfo } from '@shared/models';
+import { AboutMe } from './about-me/about-me';
 import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-layout',
-  imports: [Hero, Footer],
+  imports: [Hero, Footer, AboutMe],
   templateUrl: './layout.html',
 })
 export class Layout {
